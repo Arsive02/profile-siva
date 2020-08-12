873bc0b5c11769f9c673264e4bf387708547af0c
Arsive <55843765+Arsive02@users.noreply.github.com>
2020-08-12 22:40:42 +0530

    created the php file for contact form

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_from = 'assasinsiva8888@gmail.com';
+
+	$email_subject = "new form submission";
+
+	$email_body = "User name: $name.\n".
+					"User Email: $visitor_email.\n".
+						"User Message: $message.\n".
+
+	$to = "sivaparkour2@gmail.com"
+
+	$headers = "From: $email_from \r\n";
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location: index.html");
+
+?>
